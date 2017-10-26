87af01d9fe9f6bf51184edd6209d484f2229d292
叶青 <yeqing@yeqingdeMacBook-Pro.local>
2017-10-26 22:07:23 +0800

    add LoginActivity

---
+++ app/src/main/java/com/example/yeqing/logisticsapp/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.yeqing.logisticsapp.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.yeqing.logisticsapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
