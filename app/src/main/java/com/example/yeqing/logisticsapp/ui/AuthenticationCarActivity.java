89fdc361b62484d7606ced394e5c51ba5b403649
kasiea <278779467@qq.com>
2017-11-01 21:33:03 +0800

    add Some Activity UI

--- app/src/main/java/com/example/yeqing/logisticsapp/ui/AuthenticationCarActivity.java
+++ app/src/main/java/com/example/yeqing/logisticsapp/ui/AuthenticationCarActivity.java
@@ -1,19 +1,15 @@
 package com.example.yeqing.logisticsapp.ui;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.yeqing.logisticsapp.R;
 
-public class MainActivity extends AppCompatActivity {
+public class AuthenticationCarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-//        Intent intent = new Intent(this.getApplicationContext(), AuthenticationDriverActivity.class);
-//        startActivity(intent);
+        setContentView(R.layout.activity_authentication_car);
     }
 }
